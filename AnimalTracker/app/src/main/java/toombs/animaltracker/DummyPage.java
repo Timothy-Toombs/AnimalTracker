abc95b87bc43372aa7d09f055fddf2b11d162e43
Chris Sclipei <csclipei@ucsc.edu>
2020-11-18 17:25:04 -0800

    finished AnimalUtil class

--- AnimalTracker/app/src/main/java/toombs/animaltracker/DummyPage.java
+++ AnimalTracker/app/src/main/java/toombs/animaltracker/DummyPage.java
@@ -1,6 +1,8 @@
 package toombs.animaltracker;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -47,16 +49,18 @@ import toombs.animaltracker.wrappers.infoClasses.LogInfo;
 
 public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private ImageView animalPicture;
+    private AlertDialog dialog, dialog2;
+    private ImageView animalPicture, galleryIntent, cameraIntent;
     private EditText petName, scientificName, commonName, age, weight;
     private Button add, cancel, search;
     private Spinner spinner;
     private String animalSex;
+    private static final int TAKE_IMAGE = 0;
     private static final int PICK_IMAGE = 1;
     static int g = 0;
     private Uri imageUri;
     private byte[] byteArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,10 +81,10 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
             //NOTE THAT THIS ONLY CONFIRMS THAT THE INFO LOADS AND SAVES.
             @Override
             public void onClick(View view) { //TODO this is just a test of functionality. du
-                WrapperUtil.insertLogInfo(getApplicationContext(),"DAISY-" + WrapperUtil.logPathDirName,new LogInfoWrapper(DummyPage.g, Wrapper.WRAPPER_START_SENTINEL,Wrapper.WRAPPER_END_SENTINEL,
-                        new LogInfo(new GregorianCalendar(),"THIS IS THE LOG MESSAGE")));
-                LogInfoWrapper wrapper = WrapperUtil.loadLogInfoWrapper(getApplicationContext(),"DAISY-"+ WrapperUtil.logPathDirName,DummyPage.g);
-                Logger.getAnonymousLogger().info("THIS IS THE ID OF THE PREV AND THE NEXT, RESPECTIVELY: " +wrapper.getPrevID() + ", " + wrapper.getNextID());
+                WrapperUtil.insertLogInfo(getApplicationContext(), "DAISY-" + WrapperUtil.logPathDirName, new LogInfoWrapper(DummyPage.g, Wrapper.WRAPPER_START_SENTINEL, Wrapper.WRAPPER_END_SENTINEL,
+                        new LogInfo(new GregorianCalendar(), "THIS IS THE LOG MESSAGE")));
+                LogInfoWrapper wrapper = WrapperUtil.loadLogInfoWrapper(getApplicationContext(), "DAISY-" + WrapperUtil.logPathDirName, DummyPage.g);
+                Logger.getAnonymousLogger().info("THIS IS THE ID OF THE PREV AND THE NEXT, RESPECTIVELY: " + wrapper.getPrevID() + ", " + wrapper.getNextID());
                 DummyPage.g++;
             }
         }); //TODO REFACTOR THIS >:)
@@ -104,6 +108,7 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
         private String current = "";
         private String ddmmyyyy = "DDMMYYYY";
         private GregorianCalendar calendar = new GregorianCalendar();
+
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -126,12 +131,12 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
                 if (clean.length() < 8) {
                     clean = clean + ddmmyyyy.substring(clean.length());
                 } else {
-                    int day = Integer.parseInt(clean.substring(0,2));
-                    int month = Integer.parseInt(clean.substring(2,4));
-                    int year = Integer.parseInt(clean.substring(4,8));
+                    int day = Integer.parseInt(clean.substring(0, 2));
+                    int month = Integer.parseInt(clean.substring(2, 4));
+                    int year = Integer.parseInt(clean.substring(4, 8));
 
                     month = month < 1 ? 1 : Math.min(month, 12);
-                    calendar.set(Calendar.MONTH, month-1);
+                    calendar.set(Calendar.MONTH, month - 1);
                     year = (year < 1900) ? 1900 : Math.min(year, 2100);
                     calendar.set(Calendar.YEAR, year);
 
@@ -155,7 +160,7 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
         }
     };
 
-    public void addNewAnimalDialog(){
+    public void addNewAnimalDialog() {
         dialogBuilder = new AlertDialog.Builder(this);
         final View animalPopupView = getLayoutInflater().inflate(R.layout.popup, null);
 
@@ -164,11 +169,7 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
         animalPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gallery = new Intent();
-                gallery.setType("image/*");
-                gallery.setAction(Intent.ACTION_GET_CONTENT);
-
-                startActivityForResult(Intent.createChooser(gallery, "Select Picture"), PICK_IMAGE);
+                animalImage();
             }
         });
 
@@ -211,18 +212,56 @@ public class DummyPage extends AppCompatActivity implements AdapterView.OnItemSe
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
-            imageUri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                animalPicture.setImageBitmap(bitmap);
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                byteArray = stream.toByteArray();
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
+        Bitmap bitmap;
+        imageUri = data.getData();
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    bitmap = (Bitmap) extras.get("data");
+                    animalPicture.setImageBitmap(bitmap);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    byteArray = stream.toByteArray();
+                }
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    animalPicture.setImageURI(imageUri);
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                        byteArray = stream.toByteArray();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
         }
+    }
+
+    public void animalImage() {
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Add a picture");
+
+        dialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (options[which].equals("Take Photo")) {
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, TAKE_IMAGE);
+                } else if (options[which].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, PICK_IMAGE);
+                } else if (options[which].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        dialogBuilder.show();
     }
 
     @Override
--- AnimalTracker/app/src/main/java/toombs/animaltracker/animals/AnimalUtil.java
+++ AnimalTracker/app/src/main/java/toombs/animaltracker/animals/AnimalUtil.java
@@ -1,51 +1,116 @@
-//package toombs.animaltracker.animals;
-//
-//import android.content.Context;
-//import android.util.Log;
-//
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//
-//import toombs.animaltracker.wrappers.Wrapper;
-//import toombs.animaltracker.wrappers.WrapperUtil;
-//
-//public class AnimalUtil {
-//
-//    static private void serializeAnimal(Context context, Animal animal)  {
-//        try {
-//            FileOutputStream fos = context.openFileOutput(animal.getPetName(), Context.MODE_PRIVATE);
-//            ObjectOutputStream os = new ObjectOutputStream(fos);
-//            os.writeObject(animal);
-//            os.close();
-//        }catch (FileNotFoundException e) {
-//            Log.e("error:FileNotFoundException",e.getMessage());ls
-//        } catch (IOException e) {
-//            Log.e("error:IOException",e.getMessage());
-//        }
-//    }
-//
-//    private static  Animal loadAnimal(Context context, String pathName) {
-//        Animal animal = null;
-//        try {
-//            FileInputStream fis = context.openFileInput(pathName);
-//            ObjectInputStream is = new ObjectInputStream(fis);
-//
-//            wrapper = (Wrapper) is.readObject();
-//
-//            is.close();
-//            fis.close();
-//        }catch (FileNotFoundException e) {
-//            Log.e("error:FileNotFoundException",e.getMessage());
-//        } catch (IOException e) {
-//            Log.e("error:IOException",e.getMessage());
-//        } catch (ClassNotFoundException e) {
-//            Log.e("error:ClassNotFoundException",e.getMessage());
-//        }
-//
-//        return wrapper;
-//    }
-//}
+package toombs.animaltracker.animals;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
+
+
+public class AnimalUtil {
+    private static final String animalSetPath = "animalSet";
+
+    public static void insertAnimal(Context context, Animal animal) {
+        HashSet<String> animalSet = loadAnimalSet(context, animalSetPath);
+        if (animalSet == null) {
+            animalSet = new HashSet<>();
+        }
+        String hashName = hashedAnimalName(animalToByteArray(animal));
+        animalSet.add(hashName);
+        serializeObject(context, animalSetPath, animalSet);
+        serializeObject(context, hashName, animal);
+    }
+
+    public static void removeAnimal(Context context, Animal animal) {
+        HashSet<String> animalSet = loadAnimalSet(context, animalSetPath);
+        String hashName = hashedAnimalName(animalToByteArray(animal));
+        if (!animalSet.isEmpty()) {
+            animalSet.remove(hashName);
+        }
+        context.deleteFile(hashName);
+        serializeObject(context, animalSetPath, animalSet);
+    }
+
+    private static String hashedAnimalName(byte[] byteArray) {
+        StringBuffer str = new StringBuffer();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(byteArray);
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashName = no.toString(16);
+            str.append(hashName);
+            while (str.length() < 32) {
+                str.insert(0, "0");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("error:NoSuchAlgorithmException",e.getMessage());
+        }
+        return str.toString();
+    }
+
+    private static byte[] animalToByteArray(Animal animal) {
+        byte[] animalBytes = null;
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(out);
+            os.writeObject(animal);
+            os.flush();
+            animalBytes = out.toByteArray();
+            out.close();
+        } catch (IOException e){
+            Log.e("error:IOException",e.getMessage());
+        }
+        return animalBytes;
+    }
+
+    private static void serializeObject(Context context, String pathName, Object object) {
+        try {
+            FileOutputStream fos = context.openFileOutput(pathName, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(object);
+            os.close();
+        } catch (FileNotFoundException e) {
+            Log.e("error:FileNotFoundException", e.getMessage());
+        } catch (IOException e) {
+            Log.e("error:IOException", e.getMessage());
+        }
+    }
+
+    private static Object loadObject(Context context, String pathName) {
+        Object object = null;
+        try {
+            FileInputStream fis = context.openFileInput(pathName);
+            ObjectInputStream is = new ObjectInputStream(fis);
+
+            object = is.readObject();
+
+            is.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.e("error:FileNotFoundException", e.getMessage());
+        } catch (IOException e) {
+            Log.e("error:IOException", e.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("error:ClassNotFoundException", e.getMessage());
+        }
+
+        return object;
+    }
+
+    public static HashSet<String> loadAnimalSet(Context context, String pathName) {
+        return (HashSet<String>) loadObject(context, pathName);
+    }
+
+    public static Animal loadAnimal(Context context, String pathName) {
+        return (Animal) loadObject(context, pathName);
+    }
+}
